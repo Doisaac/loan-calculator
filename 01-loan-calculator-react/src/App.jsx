f71424830e765f30e46026db1406b3ab5e52ee8f
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-11-01 22:41:49 -0600

    🗯️ add range input with state

--- 01-loan-calculator-react/src/App.jsx
+++ 01-loan-calculator-react/src/App.jsx
@@ -1,12 +1,38 @@
+import { useState } from 'react';
 import Header from './components/Header.jsx';
 
 function App() {
   // Código JavaScript
-  const hola = 'Hola Mundo';
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
+
+  // Hooks
+  const [cantidad, setCantidad] = useState(10000);
+
+  function handleChange(e) {
+    setCantidad(e.target.value);
+  }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
+
+      <input 
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={cantidad}
+        type="range"
+        className="w-full h-5 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={ handleChange }
+      />
+
+      <p className="text-center my-10 text-2xl font-extrabold text-indigo-600"> 
+        {cantidad}
+      </p>
+
     </div>
   )
 }
