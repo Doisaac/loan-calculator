6e29476f6ea6d3efea483beb9d4d8a1a4d5f0568
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-11-03 22:38:51 -0600

    🥊 add header component

--- 02-loan-calculator-vue/src/App.vue
+++ 02-loan-calculator-vue/src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+  import Header from './components/Header.vue';
 </script>
 
 <template>
-  <h1>Hola Mundo</h1>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <Header />
+  </div>
 </template>
 
